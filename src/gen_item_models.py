29909006df20f777ead70551d215d882560dadba
Axallia <axathearcticfox@gmail.com>
2025-03-17 17:52:02 +0200

    added the model and itemstate for amethug pickave

--- src/gen_item_models.py
+++ src/gen_item_models.py
@@ -1,13 +1,25 @@
 from minecraft_model_generation.simple_parented import gen_basic_pareneted as gen
 
+if 0:
+    items_fiiiiiishhh:list[str] = [
+        'angry', 'askri', 'blind', 'cassie', 'clownfish', 'dirt', 'fishaxe', 'gledder', 'goldfish', 'harry', 'ice', 'jm', 'obsidian', 'og', 'piranha', 'swordfish', 'tosanoide', 'transgender', 'tuna'
+    ]
 
-items_regular:list[str] = [
-    'angry', 'askri', 'blind', 'cassie', 'clownfish', 'dirt', 'fishaxe', 'gledder', 'goldfish', 'harry', 'ice', 'jm', 'obsidian', 'og', 'piranha', 'swordfish', 'tosanoide', 'transgender', 'tuna'
-]
+
+    gen(
+        'assets/frostbite/models/item/fish/',
+        items_fiiiiiishhh,
+        'frostbite:item/fish/'
+    )
+
+if 1:
+    items_pickaxes:list[str] = [
+        'amethyst'
+    ]
 
 
-gen(
-    'assets/frostbite/models/item/fish/',
-    items_regular,
-    'frostbite:item/fish/'
-)
+    gen(
+        'assets/frostbite/models/item/tool/pickaxe/',
+        items_pickaxes,
+        'frostbite:item/tool/pickaxe/'
+    )
--- src/gen_itemstates.py
+++ src/gen_itemstates.py
@@ -1,12 +1,23 @@
 from minecraft_model_generation.simple_itemstate import gen_basic_itemstates as gen
 
+if 0:
+    items_fish:list[str] = [
+        'angry', 'askri', 'blind', 'cassie', 'clownfish', 'dirt', 'fishaxe', 'gledder', 'goldfish', 'harry', 'ice', 'jm', 'obsidian', 'og', 'piranha', 'swordfish', 'tosanoide', 'transgender', 'tuna'
+    ]
 
-items_fish:list[str] = [
-    'angry', 'askri', 'blind', 'cassie', 'clownfish', 'dirt', 'fishaxe', 'gledder', 'goldfish', 'harry', 'ice', 'jm', 'obsidian', 'og', 'piranha', 'swordfish', 'tosanoide', 'transgender', 'tuna'
-]
+    gen(
+        'assets/frostbite/items/fish/',
+        items_fish,
+        'frostbite:item/fish/'
+    )
 
-gen(
-    'assets/frostbite/items/fish/',
-    items_fish,
-    'frostbite:item/fish/'
-)
+if 1:
+    items_picamds:list[str] = [
+        'amethyst'
+    ]
+
+    gen(
+        'assets/frostbite/items/tool/pickaxe/',
+        items_picamds,
+        'frostbite:item/tool/pickaxe/'
+    )
