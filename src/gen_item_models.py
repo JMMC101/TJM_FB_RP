b08672c5e96886db373e28da8fe3d5befef284e9
JMMC101 <jakemillard2006@gmail.com>
2025-04-28 23:19:57 +0100

    cleaning

--- src/gen_item_models.py
+++ src/gen_item_models.py
@@ -12,6 +12,7 @@ if 0:
         'frostbite:item/fish/'
     )
 
+
 if 1:
     items_pickaxes:list[str] = [
         'amethyst'
@@ -22,4 +23,54 @@ if 1:
         'assets/frostbite/models/item/tool/pickaxe/',
         items_pickaxes,
         'frostbite:item/tool/pickaxe/'
+    )
+
+if 1:
+    items_swords:list[str] = [
+        'wooden.shattered','wooden.baton','stone.healthy','stone.dagger','lightsaber','iron.longsword','iron.longsword.heavy'
+    ]
+
+
+    gen(
+        'assets/frostbite/models/item/weapon/sword/',
+        items_swords,
+        'frostbite:item/weapon/sword/'
+    )
+
+
+
+if 1:
+    items_elytra_pieces:list[str] = [
+        '0','1','2','3','4','5','6','7','8'
+    ]
+
+
+    gen(
+        'assets/frostbite/models/item/other/broken_elytra_part/',
+        items_elytra_pieces,
+        'frostbite:item/other/broken_elytra_part/'
+    )
+
+
+
+
+if 1:
+    states_crossbow:list[str] = [
+        'standby',
+        'pulling_0',
+        'pulling_1',
+        'pulling_2',
+        'arrow',
+    ]
+
+
+    gen(
+        'assets/frostbite/models/item/weapon/crossbow/broken/',
+        states_crossbow,
+        'assets/frostbite/textures/item/weapon/crossbow/broken/'
+    )
+    gen(
+        'assets/frostbite/models/item/weapon/crossbow/minigun/',
+        states_crossbow,
+        'assets/frostbite/textures/item/weapon/crossbow/minigun/'
     )
